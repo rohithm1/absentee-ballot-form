dbbec3d3a23c8ee2970702510d53d00e015b0c67
Archita Harathi <archita.hi@gmail.com>
2020-03-27 19:15:32 -0700

    Archita creating footer

---
+++ src/components/pagefooter.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../styles/footer.scss'
+
+class PageFooter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="footer-container">
+        <p>Sponsored by Riley Gordon for State Representative -- Contact at <a href="mailto:info@rileyfornh.com">info@rileyfornh.com</a></p>
+        <p>Offical Form found at NH.gov</p>
+        <p>Designed and Developed by Dartmouth students</p>
+
+      </div>
+    );
+  }
+}
+
+export default PageFooter;
